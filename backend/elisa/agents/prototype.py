43bdd0c59eee4f6b9b6487e16c4c19093dc7d21b
Dennis Schulmeister-Zimolong <dennis@patk.org>
2025-06-30 22:19:53 +0200

    Backend: Redesign chat protocol

--- backend/elisa/agents/prototype.py
+++ backend/elisa/agents/prototype.py
@@ -6,7 +6,7 @@
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 
-import json, os, typing, uuid
+import json, os, uuid
 
 from langchain.chat_models       import init_chat_model
 from langchain_core.prompts      import ChatPromptTemplate
@@ -18,64 +18,113 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph             import START
 from langgraph.graph             import StateGraph
 from typeguard                   import check_type
+from typing                      import Literal
+from typing                      import NotRequired
+from typing                      import Optional
+from typing                      import Protocol
+from typing                      import TypedDict
 
-class ChatMessage(typing.TypedDict):
+class ChatMessage(TypedDict):
     """
-    A single chat message from user to agent and vice-versa.
+    A single chat message as exchanged between frontend and backend or stored
+    in the message history.
     """
-    id:   uuid.UUID
-    role: typing.Literal["user", "agent"]
-    type: typing.Literal["say", "think"]
+    id:   NotRequired[uuid.UUID]
+    name: Literal["User", "Agent", "Summarizer"]
+    type: Literal["say", "think"]
     text: str
 
-class QuizQuestion(typing.TypedDict):
+class QuizQuestion(TypedDict):
     """
     Question type for the quiz activity.
     """
-    question: str
-    answers:  list[str]
-    correct:  int
+    question:     str
+    answers:      list[str]
+    correct:      int
+    given_answer: NotRequired[str]
 
-class QuizActivity(typing.TypedDict):
+class StartQuizActivity(TypedDict):
     """
     Quiz activity where each question has a text and several answers of which
     exactly one is correct.
     """
-    activity:  typing.Literal["quiz"]
+    activity:  Literal["quiz"]
     subject:   str
     level:     str
     questions: list[QuizQuestion]
 
-ActivityData = QuizActivity #|OtherActivity|YetAnotherActivity
-"""The actual activity data for each activity type"""
+class EndQuizActivity(TypedDict):
+    """
+    Request for LLM feedback at the end of a quiz activity. The questions list
+    must contain the given answers as a string (not index).
+    """
+    questions: list[QuizQuestion]
+    language:  str
 
-class SendChatMessageCallback(typing.Protocol):
+StartActivityData = StartQuizActivity #|OtherActivity|YetAnotherActivity
+"""Data to start a new activity"""
+
+EndActivityData = EndQuizActivity
+"""Received data at the end of an activity"""
+
+class SendChatMessageCallback(Protocol):
     """
     Callback function that sends a chat message to the client.
     """
-    async def __call__(self, message: ChatMessage) -> None: ...
+    async def __call__(self, message: ChatMessage): ...
 
-class SendStartActivityCallback(typing.Protocol):
+class SendStartActivityCallback(Protocol):
     """
     Callback function that sends an interactive activity to the client.
     """
-    async def __call__(self, data: ActivityData) -> None: ...
+    async def __call__(self, data: StartActivityData): ...
 
-class _State(typing.TypedDict):
+class SendConversationStateCallback(Protocol):
     """
-    Shared state for all nodes in the LLM graph.
+    Callback function that sends the conversation state to the client to
+    be persisted.
     """
-    buffer: list
+    async def __call__(self, data: "PersistedConversation"): ...
+
+class PersistedConversation(TypedDict):
+    """
+    A whole conversation thread persisted to resume the conversation later
+    with a new client connection. Basically contains all non-transient fields
+    of the workflow state and the thread id.
+    """
+    thread_id: str
+    """Thread id to distinguish conversations"""
+
+    buffer: list[ChatMessage]
     """Past messages not yet contained in the summary."""
 
     summary: str
     """Constantly updated summary of the past conversation"""
 
-    user_input: str
-    """Currently processed user input message"""
+    language: str
+    """Currently selected language by the user."""
+
+class _State(TypedDict):
+    """
+    Shared state for all nodes in the LLM graph.
+    """
+    _reset: bool
+    """Private key to clear the state when a new conversation shall be started"""
+
+    _resume: NotRequired[PersistedConversation]
+    """Private key to inject the state when an old conversation shall be resumed"""
+
+    buffer: list[ChatMessage]
+    """Past messages not yet contained in the summary."""
+
+    summary: str
+    """Constantly updated summary of the past conversation"""
 
     language: str
     """Currently selected language by the user."""
+
+    user_input: str
+    """Currently processed user input message"""
 
 class PrototypeAgent:
     """
@@ -83,8 +132,23 @@ class PrototypeAgent:
     simple agent that asks the user which topic to learn, answers questions and
     creates simple quizzes.
     """
-    def __init__(self):
-        # Individual thread id to distinguish concurrent conversations
+    def __init__(
+        self,
+        send_chat_message:       SendChatMessageCallback,
+        send_start_activity:     SendStartActivityCallback,
+        send_conversation_state: SendConversationStateCallback,
+    ):
+        """
+        Parameters:
+            send_chat_message:   Asynchronous callback to send a chat message to the client
+            send_start_activity: Asynchronous callback to send an activity to the client
+        """
+        # Remember callbacks
+        self._send_chat_message       = send_chat_message
+        self._send_start_activity     = send_start_activity
+        self._send_conversation_state = send_conversation_state
+
+        # Thread id to distinguish conversations
         self.thread_id = str(uuid.uuid4())
 
         # Chat model that does all the heavy work
@@ -98,7 +162,10 @@ class PrototypeAgent:
         # Nodes return state updates (only what needs to be changed).
         workflow = StateGraph(state_schema=_State)
 
-        workflow.add_edge(START, "send_user_message_to_llm")
+        workflow.add_edge(START, "restore_conversation_state")
+        workflow.add_node("restore_conversation_state", self._restore_conversation_state)
+
+        workflow.add_edge("restore_conversation_state", "send_user_message_to_llm")
         workflow.add_node("send_user_message_to_llm", self._send_user_message_to_llm)
 
         workflow.add_edge("send_user_message_to_llm", "summarize_past_conversation")
@@ -116,15 +183,46 @@ class PrototypeAgent:
         """
         self.record_learning_topic = allow
 
+    async def _restore_conversation_state(self, state: _State):
+        """
+        LangGraph node to inject the restored conversation into the workflow state,
+        when an old conversation from a previous session shall be continued.
+        """
+        if "_reset" in state and state["_reset"]:
+            self.thread_id = str(uuid.uuid4())
+
+            return {
+                "_reset":     False,
+                "_resume":    None,
+                "buffer":     [],
+                "summary":    "",
+                "language":   "",
+                "user_input": "",
+            }
+        elif "_resume" in state and state["_resume"]:
+            self.thread_id = state["_resume"].get("thread_id", str(uuid.uuid4()))
+
+            return {
+                "_reset":     False,
+                "_resume":    None,
+                "buffer":     state["_resume"].get("buffer", []),
+                "summary":    state["_resume"].get("summary", ""),
+                "language":   state["_resume"].get("language", ""),
+                "user_input": "",
+            }
+
     async def _send_user_message_to_llm(self, state: _State):
         """
         LangGraph node that calls the LLM to send the system prompt, the summary of the
         previous conversation and the latest user message to the LLM to generate a reply.
         """
+        if not "user_input" in state or not state["user_input"]:
+            return {}
+        
         prompt_template = ChatPromptTemplate.from_messages([
             SystemMessagePromptTemplate.from_template(_SYSTEM_PROMPT),
             SystemMessagePromptTemplate.from_template(_SUMMARY_PROMPT if state.get("summary", "") else ""),
-            HumanMessagePromptTemplate.from_template(_USER_MESSAGE),
+            HumanMessagePromptTemplate.from_template(_USER_PROMPT),
         ])
 
         prompt = prompt_template.invoke(dict(state))
@@ -142,8 +240,8 @@ class PrototypeAgent:
             # be streamed to the client.
             "buffer": [
                 *state.get("buffer", []),
-                {"type": "human", "content": state["user_input"]},
-                {"type": "agent", "content": response.content},
+                {"name": "User",   "type": "say",  "text": state["user_input"]},
+                {"name": "Agent",  "type": "say",  "text": response.content},
             ],
         }
     
@@ -154,10 +252,13 @@ class PrototypeAgent:
         context. We use this strategy because a simple message buffer has shown that the LLM often
         repeats itself by repeating parts of the previous answers with each message.
         """
+        if not "user_input" in state or not state["user_input"]:
+            return {}
+        
         if not state.get("buffer", []):
             return {"summary": ""}
         
-        template = _EXTEND_SUMMARY_MESSAGE if state.get("summary", "") else _NEW_SUMMARY_MESSAGE
+        template = _EXTEND_SUMMARY_PROMPT if state.get("summary", "") else _NEW_SUMMARY_PROMPT
         prompt_template = PromptTemplate(input_variables=["buffer", "summary", "language"], template=template)
 
         prompt = prompt_template.invoke({
@@ -169,13 +270,19 @@ class PrototypeAgent:
         response = await self.chat_model.ainvoke(prompt)
         return {"summary": response.content, "buffer": []}
 
-    async def invoke_with_new_user_message(
-        self,
-        text:                str,
-        language:            str,
-        send_chat_message:   SendChatMessageCallback,
-        send_start_activity: SendStartActivityCallback,
-    ) -> None:
+    async def start_conversation(self):
+        """
+        Reset internal state to start a completely new conversation.
+        """
+        await self.app.ainvoke({"_reset": True})
+
+    async def resume_conversation(self, conversation: PersistedConversation):
+        """
+        Reset internal state continue a previous conversation from an old session.
+        """
+        await self.app.ainvoke({"_resume": conversation})
+
+    async def invoke_with_new_user_message(self, text: str, language: str):
         """
         Called from the websocket handler to invoke the graph with another user message.
 
@@ -185,6 +292,7 @@ class PrototypeAgent:
             send_chat_message:   Asynchronous callback to send a chat message to the client
             send_start_activity: Asynchronous callback to send an activity to the client
         """
+        # Invoke workflow and stream out response
         reply_id    = uuid.uuid4()
         reply_text  = ""
         json_blocks = []
@@ -220,7 +328,7 @@ class PrototypeAgent:
                         consume_chunk(remaining_chunk)
 
         async for chunk, metadata in self.app.astream(
-            {"user_input": text, "language": language},
+            {"user_input": text, "language": language, "_reset": False},
             {"configurable": {"thread_id": self.thread_id}},
 
             # CAVEAT 1: This makes the workflow stream out all LLM responses. Only then do we
@@ -237,21 +345,40 @@ class PrototypeAgent:
             and metadata["langgraph_node"] == "send_user_message_to_llm": # type: ignore
                 consume_chunk(chunk.content) # type: ignore
 
-                await send_chat_message(ChatMessage(
+                await self._send_chat_message(ChatMessage(
                     id   = reply_id,
-                    role = "agent",
+                    name = "Agent",
                     type = "say",
-                    text = reply_text
+                    text = reply_text,
                 ))
         
+        # Start new activity if the LLM generated one
         for json_block in json_blocks:
             try:
                 quiz_activity = json.loads(json_block)
-                check_type(quiz_activity, ActivityData)
-                await send_start_activity(quiz_activity)
+                check_type(quiz_activity, StartActivityData)
+                await self._send_start_activity(quiz_activity)
             except TypeError:
                 # Ignore: LLM didn't create the correct JSON structure
                 pass
+        
+        # Send conversation state to the client to be persisted
+        state = await self.app.aget_state({"configurable": {"thread_id": self.thread_id}})
+
+        await self._send_conversation_state({
+            "thread_id": self.thread_id,
+            "buffer":    state.values.get("buffer", []),
+            "summary":   state.values.get("summary", ""),
+            "language":  state.values.get("language", ""),
+        })
+    
+    async def generate_feedback_for_activity(self, activity_data: EndActivityData):
+        """
+        Request for LLM feedback at the end of a quiz activity.
+        """
+        prompt_template = PromptTemplate(input_variables=["questions"], template=_FEEDBACK_PROMPT)
+        prompt = prompt_template.invoke({"questions": activity_data["questions"]})
+        await self.invoke_with_new_user_message(str(prompt), activity_data["language"])
 
 _SYSTEM_PROMPT="""
 # Procedure
@@ -260,17 +387,15 @@ You are ELISA - an interactive learning tutor who supports me in my learning and
 Please stick to the following procedure:
 
 1. Introduce yourself and ask me what my name is.
-2. Say hello to me. But please only once and then never again!
-3. Ask me what subject I want to learn and how well I already know it.
-4. Create suitable quiz questions and answers that are adapted to my level of knowledge.
-5. Ask me if everything is clear or if you should give me some hints for the quiz.
+2. Ask me what subject I want to learn and how well I already know it.
+3. Create suitable quiz questions and answers that are adapted to my level of knowledge.
+4. Ask me if everything is clear or if you should give me some hints for the quiz.
    Also tell me that you will give detailed feedback after I answered all questions.
-6. Later I will tell you my answers so that you can give me feedback.
-7. At the end, ask me if I want you to create more quiz questions (maybe more difficult ones now),
+5. Later I will tell you my answers so that you can give me feedback.
+6. At the end, ask me if I want you to create more quiz questions (maybe more difficult ones now),
    or if I want to learn a new topic.
 
-If I decide in favour of a new topic or more quiz questions, go back to step 4: Creating the quiz questions.
-Otherwise, politely say goodbye and offer that I can always come back for more.
+If I decide in favour of a new topic or more quiz questions, go back to step 3: Creating the quiz questions.
 
 # Quiz questions
 
@@ -308,7 +433,7 @@ This is a summary of our conversation to date as a reference for you: {summary}
 Please don't repeat what we already discussed before, unless I am explicitly asking for it.
 """
 
-_USER_MESSAGE = """
+_USER_PROMPT = """
 # Hints
 
 Sometimes I will try to elicit the correct answer for the quiz, but you are not allowed to tell me.
@@ -323,7 +448,7 @@ Just give me hints or politely refuse to answer, if I try to cheat.
 Please reply in language: {language}
 """
 
-_NEW_SUMMARY_MESSAGE = """
+_NEW_SUMMARY_PROMPT = """
 # Summarize Conversation
 
 Please create a summary of our conversation so far. Next are the full details on what you and I said.
@@ -338,7 +463,7 @@ Please write the summary in the language: {language}
 Focus on what we said last. If necessary shorten older interactions.
 """
 
-_EXTEND_SUMMARY_MESSAGE = """
+_EXTEND_SUMMARY_PROMPT = """
 # Summarized Conversation
 
 This is a summary of our conversation to date: {summary}
@@ -353,4 +478,10 @@ If you generated a quiz for me, please keep it for reference:
 # Language
 
 Please write the summary in the language: {language}
+"""
+
+_FEEDBACK_PROMPT = """
+These are my answers. Please give me feedback and explain to me, if I answered something wrong:
+
+{questions}
 """
--- backend/elisa/handlers/chat.py
+++ backend/elisa/handlers/chat.py
@@ -8,9 +8,11 @@
 
 from typeguard          import check_type
 
-from ..agents.prototype import ActivityData
 from ..agents.prototype import ChatMessage
+from ..agents.prototype import EndActivityData
+from ..agents.prototype import PersistedConversation
 from ..agents.prototype import PrototypeAgent
+from ..agents.prototype import StartActivityData
 from ..core.decorators  import handle_message
 from ..core.decorators  import websocket_handler
 from ..core.websocket   import ParentWebsocketHandler
@@ -22,8 +24,15 @@ class ChatInputMessage(WebsocketMessage):
     """
     text: str
 
-# TODO: Start conversation message to start a new thread to receive message history from client
-# TODO: End activity message to trigger LLM-created feedback
+class ResumeConversationMessage(WebsocketMessage, PersistedConversation):
+    """
+    Persisted conversation to resume from a previous session.
+    """
+
+class EndActivityMessage(WebsocketMessage, EndActivityData):
+    """
+    Request for LLM feedback at the end of an activity.
+    """
 
 @websocket_handler
 class ChatHandler:
@@ -35,7 +44,11 @@ class ChatHandler:
         Initialize client-bound handler instance.
         """
         self.parent     = parent
-        self.chat_agent = PrototypeAgent()
+        self.chat_agent = PrototypeAgent(
+            send_chat_message       = self.send_chat_message,
+            send_start_activity     = self.send_start_activity,
+            send_conversation_state = self.send_conversation_state,
+        )
     
     def notify(self, key: str, value):
         """
@@ -44,6 +57,23 @@ class ChatHandler:
         """
         if key == "record_learning_topics":
             self.chat_agent.set_record_learning_topic(value)
+
+    @handle_message("start_conversation")
+    async def handle_start_conversation(self, message: WebsocketMessage):
+        """
+        Reset conversation state to start a new conversation.
+        """
+        await self.chat_agent.start_conversation()
+
+    @handle_message("resume_conversation")
+    async def handle_resume_conversation(self, message: ResumeConversationMessage):
+        """
+        Resume previous conversation based on the received conversation state.
+        Currently, conversations can only be resumed when they are persisted
+        by the client and in a new session sent to the backend with this message,
+        as we don't yet have server-side chat persistence.
+        """
+        await self.chat_agent.resume_conversation(message)
 
     @handle_message("chat_input")
     async def handle_chat_input(self, message: ChatInputMessage):
@@ -55,12 +85,14 @@ class ChatHandler:
         text     = message.get("text", "")
         language = message.get("language", "en")
 
-        await self.chat_agent.invoke_with_new_user_message(
-            text                = text,
-            language            = language,
-            send_chat_message   = self.send_chat_message,
-            send_start_activity = self.send_start_activity
-        )
+        await self.chat_agent.invoke_with_new_user_message(text, language)
+    
+    @handle_message("end_activity")
+    async def handle_end_activity(self, message: EndActivityMessage):
+        """
+        Feed activity result to the LLM to generate a feedback message.
+        """
+        await self.chat_agent.generate_feedback_for_activity(message)
     
     async def send_chat_message(self, message: ChatMessage):
         """
@@ -68,8 +100,15 @@ class ChatHandler:
         """
         await self.parent.send_message("chat_message", message)
     
-    async def send_start_activity(self, data: ActivityData):
+    async def send_start_activity(self, data: StartActivityData):
         """
         Send message to start an interactive activity to the client.
         """
         await self.parent.send_message("start_activity", data)
+
+    async def send_conversation_state(self, data: PersistedConversation):
+        """
+        Send conversation state to the client, so that the state can be persisted
+        and the conversation be resumed in a new session.
+        """
+        await self.parent.send_message("conversation_state", data)
